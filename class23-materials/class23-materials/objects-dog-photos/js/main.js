e38f852ee80d7f28c34c8ec3221b461a0278eb0c
Jengas2015 <arejang@gmail.com>
2023-02-21 11:24:29 -0600

    Changes
    introduction to APIs

--- class23-materials/class23-materials/objects-dog-photos/js/main.js
+++ class23-materials/class23-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+fetch("https://dog.ceo/api/breeds/imag/random")
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
+        console.log(data.message)
+        document.querySelector('img').src= data.message
+    })
+
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
