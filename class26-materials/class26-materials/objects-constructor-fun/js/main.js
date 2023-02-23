f878f3f91859e5fa38f7a39e6debb6ea56394678
Jengas2015 <arejang@gmail.com>
2023-02-23 09:59:33 -0600

    object constructor practice

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+function PizzaMaker (size, toppings, crust, sauce) {
+    this.size =size
+    this.toppings = toppings
+    this.crust = crust
+    this.sauce = sauce 
+
+    this.estimatedDeliveryTime = () => {
+        console.log("it's on its way!")
+    }
+
+    this.frisbee = () => {
+        console.log("Yeet!")
+    }
+
+    this.eat = () => {
+        console.log("nomnomnom!")
+    }
+}
+
+
+let pizza = new PizzaMaker('large', ['spinach', 'garlic'], 'deep dish', 'white')
