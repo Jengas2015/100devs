6cd75946e5e9d563653c84deace39dda18cf2794
Jengas2015 <arejang@gmail.com>
2023-02-27 10:17:16 -0600

    local storage practice

--- class27-materials/class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,17 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+if(!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0)
+}
 
+document.querySelector('h2').innerText = `The score is ${localStorage.getItem('botScore')}`
+
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', addAnothaOne)
+
+function addAnothaOne(){
+    let botScoreVal = Number (localStorage.getItem('botScore'))
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
+    document.querySelector('h2').innerText = `The score is ${localStorage.getItem('botScore')}`
+}
 
