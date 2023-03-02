c8c4c58ca03e5dd5e6668ab51126ca2dfe3d4ed8
Jengas2015 <arejang@gmail.com>
2023-03-02 16:54:01 -0600

    DnD API practice

--- class28-materials/class28-materials/dnd/js/main.js
+++ class28-materials/class28-materials/dnd/js/main.js
@@ -8,7 +8,23 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data)
+        console.log(data.classes[0].name)
+        console.log(data.subclasses[0].name)
+        let classData = data.classes
+        let subclassData = data.subclasses
+        let charClass = document.querySelector('#class')
+        let charSubClass = document.querySelector('#subclass')
+
+        
+        subclassData.forEach(obj => {
+            //create an li
+            const li = document.createElement('li')
+            //add text to li
+            li.textContent = obj.name
+            //append the li to the ul
+            document.querySelector('ul').appendChild(li)
+        })
       })
       .catch(err => {
           console.log(`error ${err}`)
