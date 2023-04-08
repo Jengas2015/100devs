ee54ce9becd2f7cdbb79b7f77a9d4b815d9d43d6
Jengas2015 <arejang@gmail.com>
2023-04-07 23:16:56 -0500

    backend practice for class 39

---
+++ class39-materials/rap-names-api/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, IL'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (request, response) => {
+    response.json(rappers)
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}. Betta go catch it!`)
+})
